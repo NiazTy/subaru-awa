d256d53eefeaf8c528429d5aca3a94990e409ebf
Kiyzan <mzpnzakky@gmail.com>
2024-05-30 13:37:08 +0700

    modified: update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     fontFamily: {
       "anton": [ "Anton", "sans-serif" ],
-      "courier-prime": [ "Courier Prime", "serif" ],
-      "ibm-plex-sans": [ "IBM Plex Sans", "sans-serif" ],
+      "courier-prime": [ "'Courier Prime'", "serif" ],
+      "ibm-plex-sans": [ "'IBM Plex Sans'", "sans-serif" ]
     },
     extend: {
       colors: {
-        "background": "#E4E2DD",
-        "paragraph": "#DB4A2B",
+        "background": "#FDFEFE",
+        "paragraph": "#98A1C4",
         "white-snow": "#DB4A2B"
       },
     },
